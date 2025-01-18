c1b6dbb00151883be25dff50972bf6a843ef3009
Jothika2001 <mjothi680@gmail.com>
2025-01-18 23:13:02 +0530

    vercel updated

--- server/server.js
+++ server/server.js
@@ -255,7 +255,7 @@ const adminUser = {
 // MongoDB connection with retry logic
 const connectDB = async () => {
   try {
-    await mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true });
+    await mongoose.connect(mongoURI);
     console.log("Connected to MongoDB");
   } catch (error) {
     console.error("Error connecting to MongoDB:", error);
